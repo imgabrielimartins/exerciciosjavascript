f2ca5c8877bd3bc27c06143737f5d3a559908dbc
Gabrieli <gabrielidelimamartins@gmail.com>
2026-03-25 15:47:57 -0300

    exercicio 1

---
+++ exercicio1.js
@@ -0,0 +1,14 @@
+/* 
+Escreva um programa que recebe um número inteiro e verifica se ele é par 
+ou ímpar utilizando uma estrutura de controle if.
+*/
+
+const readlineSync = require('readline-sync');
+
+let numero = readlineSync.questionInt('Digite um numero: ');
+
+if(numero % 2 === 0){
+    console.log('É par!');
+} else{
+    console.log('É ímpar!');
+} 
